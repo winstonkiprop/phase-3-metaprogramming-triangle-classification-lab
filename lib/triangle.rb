12877012696779c0feb7f8ddfdadb176e7c6d80f
winston kiprop <winstonekip50@gmail.com>
2022-08-23 11:10:48 +0300

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
   # write code here
+  attr_reader :length, :width, :heigth
+
+  def initialize(length,width,heigth)
+    @length = length
+    @width = width
+    @heigth = heigth
+  end
+
+  def kind
+    if length == width && width == heigth
+      return :equilateral
+    elsif length == width || width == heigth || length == heigth
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
+
+  def validate_triangle
+    real_triangle =  [(length + width > heigth), (length + heigth > width), (width + heigth > length)]
+      [length, width, heigth].each do |side|
+        real_triangle << false if side <= 0 
+      raise TriangleError if real_triangle.include?(false)
+      end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Triangle inequality failed"
+    end
+  end
 end
